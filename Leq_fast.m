7e2e1610eeeee0c20c5689ce152bd749f3564d94
beedholm <beedholm@biology.au.dk>
2018-08-10 09:33:25 +0200

    Add files via upload

---
+++ Leq_fast.m
@@ -0,0 +1,51 @@
+function Leq=Leq_fast(sig,fs,tau,type)
+
+%function Leq=Leq_fast(sig,fs,tau,type)
+%Computes running rms-average of signal sig, sampled with sample rate sr
+%tau is the integration time in seconds
+%Default value for tau is 125 ms
+%Kernel is either exponential decay (default, and type ='exponetial', or
+%rectangular (type = 'rectangular')
+%output is an array of the same size as the input. 
+
+%Aarhus University, Jakob Tougaard and Kristian Beedholm, Jan. 2018 
+%jat@bios.au.dk
+
+if size(sig,1)==1    %Flip input if horizontal array
+    sig=sig';
+    horizontal=true;
+else
+    horizontal=false;
+end
+
+if nargin<3
+    tau=0.125;  %default value 125 ms
+end
+
+if nargin<4    %default type is exponential
+    type='e';
+end
+
+L=floor(fs*tau);    %Time constant in samples
+if type(1)=='r'     %rectangular kernel
+    if L>length(sig)
+        error('Signal must be longer than tau')
+    else
+        w=[ones(L,1);zeros(length(sig)-L,1)];
+    end
+else                %exponential kernel
+    if L*5>length(sig)
+        error('Signal must be longer than five times tau')
+    else
+        w=exp(-(0:length(sig)-1)/(tau*fs))';  
+    end
+end
+
+%convolution)
+Leq=sqrt((ifft(fft(sig.^2).*fft(w)))/L);
+
+if horizontal   %flip output back to horizontal if input is a horizontal array
+    Leq=Leq';
+end
+
+
